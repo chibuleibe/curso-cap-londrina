9f1d4aea7e079bd7eaa575ea4caf7835a28e3441
chibuleibe <chibuleibe@gmail.com>
2024-05-20 23:37:25 -0300

    Add files via upload

---
+++ helloworld.py
@@ -0,0 +1,11 @@
+nome = "Pedro Forlan"
+idade = 24
+demolay = "senior"
+dinheiro = 124.21
+
+
+print("Meu nome é: ", nome)
+print("Minha idade é: ", idade)
+print("Eu sou ", demolay)
+print("Eu tenho na minha conta R$", dinheiro)
+
